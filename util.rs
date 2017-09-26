775d80cc33ea6215bf21b5cb0a8e98ae64b82c4b
aimy <kawamoto.takahisa@gmail.com>
2017-09-26 23:23:18 +0900

    add 2 functions

--- util.rs
+++ util.rs
@@ -14,6 +14,10 @@ fn gcd(x: u32, y: u32) -> u32 {
   std::cmp::max(a, b)
 }
 
+fn is_leap (y: u32) -> bool {
+  y % 4 == 0 && (y % 100 != 0 || y % 400 == 0)
+}
+
 fn rotate_mat<T: std::clone::Clone>(mat: &Vec<Vec<T>>) -> Vec<Vec<T>> {
   let h = mat.len();
   let w = mat[0].len();
@@ -28,4 +32,25 @@ fn rotate_mat<T: std::clone::Clone>(mat: &Vec<Vec<T>>) -> Vec<Vec<T>> {
   }
 
   rot
+}
+
+fn permutations<T: Clone + std::fmt::Debug>(vec: &Vec<T>) -> Vec<Vec<T>> {
+  fn go<T: Clone + std::fmt::Debug>(vec: &Vec<T>, n: usize, m: usize, k: usize, buf: &mut Vec<usize>, acc: &mut Vec<Vec<T>>) {
+    if k == 0 {
+      let r: Vec<T> = buf.iter().map(|&i| vec[i-1].clone()).collect();
+      acc.push(r);
+    } else {
+      for x in n .. m + 1 {
+        if !buf.contains(&x) {
+          buf.push(x);
+          go(vec, n, m, k-1, buf, acc);
+          buf.pop();
+        }
+      }
+    }
+  }
+
+  let mut acc = vec![];
+  go(vec, 1, vec.len(), vec.len(), &mut vec![], &mut acc);
+  acc
 }
