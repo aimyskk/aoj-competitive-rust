3f2e3a8e0589f3628d19dab3a2c215ce68c553b3
aimy <kawamoto.takahisa@gmail.com>
2017-10-21 16:00:57 +0900

    rename all functions

--- get.rs
+++ get.rs
@@ -3,21 +3,21 @@ mod get {
   use std::io::*;
   use std::str::*;
 
-  pub fn read<T: FromStr>() -> T {
+  pub fn val<T: FromStr>() -> T {
     let mut buf = String::new();
     stdin().read_line(&mut buf).ok();
     buf.trim().parse::<T>().ok().unwrap()
   }
 
-  pub fn reads<T: FromStr>(n: usize) -> Vec<T> {
+  pub fn vals<T: FromStr>(n: usize) -> Vec<T> {
     let mut vec: Vec<T> = vec![];
     for _ in 0 .. n {
-      vec.push(read());
+      vec.push(val());
     }
     vec
   }
 
-  pub fn read_tuple<T1: FromStr, T2: FromStr>() -> (T1, T2) {
+  pub fn tuple<T1: FromStr, T2: FromStr>() -> (T1, T2) {
     let mut buf = String::new();
     stdin().read_line(&mut buf).ok();
     let mut it = buf.trim().split_whitespace();
@@ -26,15 +26,15 @@ mod get {
     (x, y)
   }
 
-  pub fn read_tuples<T1: FromStr, T2: FromStr>(n: usize) -> Vec<(T1, T2)> {
+  pub fn tuples<T1: FromStr, T2: FromStr>(n: usize) -> Vec<(T1, T2)> {
     let mut vec: Vec<(T1, T2)> = vec![];
     for _ in 0 .. n {
-      vec.push(read_tuple());
+      vec.push(tuple());
     }
     vec
   }
 
-  pub fn read_3tuple<T1: FromStr, T2: FromStr, T3: FromStr>() -> (T1, T2, T3) {
+  pub fn tuple3<T1: FromStr, T2: FromStr, T3: FromStr>() -> (T1, T2, T3) {
     let mut buf = String::new();
     stdin().read_line(&mut buf).ok();
     let mut it = buf.trim().split_whitespace();
@@ -44,24 +44,24 @@ mod get {
     (x, y, z)
   }
 
-  pub fn read_3tuples<T1: FromStr, T2: FromStr, T3: FromStr>(n: usize) -> Vec<(T1, T2, T3)> {
+  pub fn tuple3s<T1: FromStr, T2: FromStr, T3: FromStr>(n: usize) -> Vec<(T1, T2, T3)> {
     let mut vec: Vec<(T1, T2, T3)> = vec![];
     for _ in 0 .. n {
-      vec.push(read_3tuple());
+      vec.push(tuple3());
     }
     vec
   }
 
-  pub fn read_vec<T: FromStr>() -> Vec<T> {
+  pub fn list<T: FromStr>() -> Vec<T> {
     let mut buf = String::new();
     stdin().read_line(&mut buf).ok();
     buf.trim().split_whitespace().map(|t| t.parse::<T>().ok().unwrap()).collect()
   }
 
-  pub fn read_mat<T: FromStr>(h: usize) -> Vec<Vec<T>> {
+  pub fn lists<T: FromStr>(h: usize) -> Vec<Vec<T>> {
     let mut mat: Vec<Vec<T>> = vec![];
     for _ in 0 .. h {
-      mat.push(read_vec());
+      mat.push(list());
     }
     mat
   }
