52f720d2c5b5200df6ff533931a365c1138818b5
aimy <kawamoto.takahisa@gmail.com>
2017-11-17 23:45:19 +0900

    rename functions

--- put.rs
+++ put.rs
@@ -2,14 +2,14 @@
 mod put {
   use std::string::*;
 
-  pub fn print_vec<T: ToString>(vec: &Vec<T>, sep: &str) {
+  pub fn vec<T: ToString>(vec: &Vec<T>, sep: &str) {
     let out = vec.iter().map(|e| e.to_string()).collect::<Vec<_>>().as_slice().join(sep);
     println!("{}", out);
   }
 
-  pub fn print_mat<T: ToString>(mat: &Vec<Vec<T>>, sep: &str) {
+  pub fn mat<T: ToString>(mat: &Vec<Vec<T>>, sep: &str) {
     for v in mat {
-      print_vec(v, sep);
+      vec(v, sep);
     }
   }
 }
